8cfe3d909f9230c7e2387c46804a0fc42411a337
jojo-ana <joanah127@gmail.com>
2024-11-07 12:33:12 -0800

    some css and html

--- studio2/script.js
+++ studio2/script.js
@@ -1,6 +1,15 @@
 (function(){
     "use strict";
+    document.addEventListener('mousemove', function(event){
+        const spotlight = document.getElementById('spotlight');
+        const x = event.clientX;
+        const y = event.clientY;
+        
+        // Update CSS variables for the spotlight's position
+        spotlight.style.setProperty('--x', `${x}px`);
+        spotlight.style.setProperty('--y', `${y}px`);
 
+    });
 
 
 })();
